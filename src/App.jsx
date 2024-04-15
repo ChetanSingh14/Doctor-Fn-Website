a085e847f65e370b47ca207f1b5b92cb59b886fe
ChetanSingh14 <siradhanachetan14@gmail.com>
2024-04-15 21:57:03 +0530

    update in forum

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Navbar from './Components/Navbar/Navbar'
-
+import Display from './Components/Forum/display';
 
 function App() {
 
   return (
+    <>
     <div>
       {/* <h1 className='bg-gray-700'>Hey</h1> */}
       <Navbar/>
+      <Display/>
+  
     </div>
+    </>
   )
 }
 
